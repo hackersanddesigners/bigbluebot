098d400cc1d86a41537862645ffead7011db991d
André Fincato <work@andrefincato.info>
2021-01-13 18:07:09 +0100

    extend note action

--- config/label/note.js
+++ config/label/note.js
@@ -1,8 +1,10 @@
 module.exports = {
-  open: 'NOTE OPEN ARIA LABEL',
+  // open: 'NOTE OPEN ARIA LABEL',
+  open: 'app.note.title',
   close: 'app.note.hideNoteLabel',
   frame: {
-    name: 'NOTE FRAME NAME ARIA LABEL',
+    // name: 'NOTE FRAME NAME ARIA LABEL',
+    name: 'app.note.label',
     pad: 'NOTE FRAME PAD ARIA LABEL',
   },
 };
